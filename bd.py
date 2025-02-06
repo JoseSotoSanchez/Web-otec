661a1cb4c2538afae79e411953474c8f43225949
JoseSotoSanchez <64284015+JoseSotoSanchez@users.noreply.github.com>
2025-02-05 23:05:28 -0300

    actualiza app

--- bd.py
+++ bd.py
@@ -1,13 +1,14 @@
 import pymysql
 
-def obtener_conexion():
-    return pymysql.connect(host="iccapacitacionlaboral.cl"
-    , user="iccapaci1_admin"
-    , password="gQ9Pb$$PKh", 
-    db="iccapaci1_iccaplabv2")
-
 # def obtener_conexion():
-#    return pymysql.connect(host="localhost"
-#    , user="root"
-#    , password="", 
-#    db="icaplab2")
+#     return pymysql.connect(host="iccapacitacionlaboral.cl"
+#     , user="iccapaci1_admin"
+#     , password="gQ9Pb$$PKh", 
+#     db="iccapaci1_iccaplabv2")
+
+def obtener_conexion():
+   return pymysql.connect(host="127.0.0.1"
+   , user="root"
+   , password="", 
+   db="iccapaci1_iccaplabv2",
+   port=3307 )
